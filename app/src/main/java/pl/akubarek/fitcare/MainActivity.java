a4257c5232c7c502a97e1f79925d7b42f03226ba
AdamK <Adamkubarek@gmail.com>
2017-01-09 17:33:10 +0100

    beta menu added

--- app/src/main/java/pl/akubarek/fitcare/MainActivity.java
+++ app/src/main/java/pl/akubarek/fitcare/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.preferences) {
             return true;
         }
 
